112deef89d6339d357f5ccc36388eb1756178662
Dobrovolischii Olga <olly20@gmail.com>
2020-06-30 15:22:44 +0300

    header information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
