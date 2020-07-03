4d3e70b3f401ce8458aedfd7f9b9530b81b0a19a
Dobrovolischii Olga <olly20@gmail.com>
2020-07-03 16:23:38 +0300

    galerie

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
+import ImageGallery from "./components/Content/ImageGallery";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <ImageGallery />
     </div>
   );
 }
